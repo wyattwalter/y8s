f107419b262ebcf74fef19bb47fa911187d701f9
Wyatt Walter <wyattwalter@gmail.com>
2021-02-03 08:56:16 -0600

    more bootstrap work on argo

--- argocd/argocd-bootstrap.sh
+++ argocd/argocd-bootstrap.sh
@@ -3,6 +3,7 @@
 # Doing this as a standalone bootstrap because the quick start wants to pull their manifest from latest, and we only need minor patches
 
 if [ "$1" = "delete" ]; then
+  for app in $(argocd app list -o name); do argocd app delete $app; done
   kubectl delete namespace argocd
   exit 0
 fi
@@ -13,22 +14,25 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # apply any overrides in argo-patch.yaml
 kubectl patch svc argocd-server -n argocd --type merge --patch "$(cat argo-patch.yaml)"
 
-
-kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
-
-sleep 5
-
 ARGOCD_PASS=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
-
+ARGOCD_LOGIN="argocd login localhost:8443 --insecure --username admin --password ${ARGOCD_PASS}"
+echo
 echo "ArgoCD should be up and running shortly at https://localhost:8443"
 echo "Login with username 'admin' and password '${ARGOCD_PASS}'"
+echo
 
-## DO NOTHING PART
+# waits until rollout of argocd-server is complete
+kubectl rollout status -n argocd deployment.apps/argocd-server
 
-echo "poll until deployment.apps/argocd-server is up"
-
-echo "install the argocd tool"
-
-echo "login to argocd locally"
+if [ -x "$(command -v argocd)" ]; then
+  $ARGOCD_LOGIN
+else
+  echo
+  echo "Argocd cli not detected in path. Install it using instructions here and then login with:"
+  echo "$ARGOCD_LOGIN"
+  echo
+fi
 
 echo "create a demo app"
+argocd repo add --username wyattwalter --password ${GITHUB_TOKEN} https://github.com/wyattwalter/demo-apps.git
+argocd app create case-api --repo https://github.com/wyattwalter/demo-apps.git --path case-api --dest-namespace default --dest-server https://kubernetes.default.svc
